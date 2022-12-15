2c70fa413947ede2c4e64dc829b52388a18d340c
lby132 <quddbscjswow@naver.com>
2022-12-15 18:24:15 +0900

    클래스를 이용한 다이나믹 프록시 생성

--- src/test/java/me/whiteship/demospringdi/BookServiceTest.java
+++ src/test/java/me/whiteship/demospringdi/BookServiceTest.java
@@ -1,12 +1,16 @@
 package me.whiteship.demospringdi;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class BookServiceTest {
 
@@ -25,6 +29,39 @@ class BookServiceTest {
             });
     @Test
     void di() {
+        final Book book = new Book();
+        book.setTitle("spring");
+        bookService.rent(book);
+    }
+
+    @Test
+    void di2() {
+        MethodInterceptor handler = new MethodInterceptor() {
+            BookService2 bookService = new BookService2();
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects,
+                                    MethodProxy methodProxy) throws Throwable {
+                return method.invoke(bookService, objects);
+            } };
+
+        final Book book = new Book();
+        book.setTitle("spring");
+        bookService.rent(book);
+    }
+
+    @Test
+    void di3() throws Exception {
+        final Class<? extends BookService2> proxyClass = new ByteBuddy().subclass(BookService2.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    BookService2 bookService = new BookService2();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        return method.invoke(bookService, args);
+                    }
+                }))
+                .make().load(BookService2.class.getClassLoader()).getLoaded();
+        final BookService2 bookService2 = proxyClass.getConstructor(null).newInstance();
+
         final Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
